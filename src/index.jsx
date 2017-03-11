5725524c70a41ed8c957ff1ccc0e470e270cd919
James Wright <james@jamesswright.co.uk>
2017-03-11 13:41:07 +0000

    Reduced frame rate

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,6 @@ const ReactDOM = require('react-dom');
 const Timer = require('./Timer.jsx');
 
 ReactDOM.render(
-	<Timer message="Let's Time Stuff!" durationMs={5000} />,
+	<Timer message="Let's Time Stuff!" durationMs={2000} />,
 	document.querySelector('#app')
 );
